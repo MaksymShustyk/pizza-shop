71ad159209fa5f0d1e89b69a5176efe91f5fcbd4
Maksym_Shustyk <shustik2@gmail.com>
2021-11-18 23:20:25 +0200

    Створення гамбургер меню для малих розширень.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { Categories, SortPopup, PizzasBlock,  PizzasLoadingBlock} from '../compo
 
 import {setCategory, setSortBy} from '../redux/actions/filters';
 import { fetchPizzas } from '../redux/actions/pizzas';
+import { addPizzaToCart } from '../redux/actions/cart';
 
 
 const categoryNames = ['Мясные', 'Вегатерианские', 'Гриль', 'Острые', 'Закрытые'];
