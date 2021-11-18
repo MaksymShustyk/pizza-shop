d1a004670cb923f6855db065c11c49f70822139f
Maksym_Shustyk <shustik2@gmail.com>
2021-11-18 23:23:41 +0200

    Розробка адаптиву

--- src/components/PizzasBlock/index.jsx
+++ src/components/PizzasBlock/index.jsx
@@ -4,11 +4,11 @@ import PropTypes from 'prop-types'
 import Button from '../Button'
 
 
-function PizzasBlock({id, name, imageUrl, sizes, price, types, onClickAddPizza}) {
+function PizzasBlock({id, name, imageUrl, sizes, price, types, onClickAddPizza, addedCount}) {
     const typeNames = ['тонкое','традиционное']
     const sizeNames = [26,30,40]
     const [activeType, setActiveType] = React.useState(types[0])
-
+// Виправити size на index.
     const [activeSize, setActiveSize] = React.useState(sizes[0])
 
 
@@ -21,9 +21,16 @@ function PizzasBlock({id, name, imageUrl, sizes, price, types, onClickAddPizza})
     }
 
     const onAddPizza = ()=>{
-        onClickAddPizza({id, name, imageUrl, sizes, price});
+        const obj = {
+            id,
+            name,
+            imageUrl,
+            price,
+            size: activeSize,
+            type: typeNames[activeType],
+        }
+        onClickAddPizza(obj);
     };
-
     return (
         <div className="pizza-block">
                         <img src={imageUrl} alt="Hot pizza img" className="pizza-block_img"/>
@@ -70,7 +77,8 @@ function PizzasBlock({id, name, imageUrl, sizes, price, types, onClickAddPizza})
                                     <path d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"></path>
                                 </svg>
                                 <span>Добавить</span>
-                                <i>2</i>
+                                {/* {addedCount &&  */}
+                                <i>{addedCount}</i>
                             </Button>
 
                         </div>
@@ -85,6 +93,7 @@ PizzasBlock.propTypes = {
     types: PropTypes.arrayOf(PropTypes.number),
     size: PropTypes.arrayOf(PropTypes.number),
     onClickAddPizza: PropTypes.func,
+    addedCount: PropTypes.number,
 };
 
 PizzasBlock.defaultProps = {
@@ -92,6 +101,7 @@ PizzasBlock.defaultProps = {
     price: 0,
     types: [],
     size: [],
+    addedCount:0,
 }
 
 export default PizzasBlock
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -33,7 +33,7 @@ function Cart() {
                         <div className="content-item">
                             <div className="cart-item">
                                 <div className="cart-item_img">
-                                    <img src="img/pizza_image1.png" alt="Hot pizza img" className="pizza-block_img"/>
+                                    <img src="https://dodopizza.azureedge.net/static/Img/Products/f035c7f46c0844069722f2bb3ee9f113_584x584.jpeg" alt="Hot pizza img" className="pizza-block_img"/>
                                 </div>
                                 <div className="cart-item_info">
                                     <h3>Чизбургер-пицца</h3>
@@ -60,12 +60,15 @@ function Cart() {
                                             <path d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z" ></path>
                                             <path d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"></path>
                                             </svg>
+                                            <span>
+                                                Удалить пиццу
+                                            </span>
                                     </div>
                                 </div>
                             </div>
                             <div className="cart-item">
                                 <div className="cart-item_img">
-                                    <img src="img/pizza_image1.png" alt="Hot pizza img" className="pizza-block_img"/>
+                                    <img src="https://dodopizza.azureedge.net/static/Img/Products/f035c7f46c0844069722f2bb3ee9f113_584x584.jpeg" alt="Hot pizza img" className="pizza-block_img"/>
                                 </div>
                                 <div className="cart-item_info">
                                     <h3>Чизбургер-пицца</h3>
@@ -92,12 +95,15 @@ function Cart() {
                                             <path d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z" ></path>
                                             <path d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"></path>
                                             </svg>
+                                            <span>
+                                                Удалить пиццу
+                                            </span>
                                     </div>
                                 </div>
                             </div>
                             <div className="cart-item">
                                 <div className="cart-item_img">
-                                    <img src="img/pizza_image1.png" alt="Hot pizza img" className="pizza-block_img"/>
+                                    <img src="https://dodopizza.azureedge.net/static/Img/Products/f035c7f46c0844069722f2bb3ee9f113_584x584.jpeg" alt="Hot pizza img" className="pizza-block_img"/>
                                 </div>
                                 <div className="cart-item_info">
                                     <h3>Чизбургер-пицца</h3>
@@ -124,6 +130,9 @@ function Cart() {
                                             <path d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z" ></path>
                                             <path d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"></path>
                                             </svg>
+                                            <span>
+                                                Удалить пиццу
+                                            </span>
                                     </div>
                                 </div>
                             </div>
