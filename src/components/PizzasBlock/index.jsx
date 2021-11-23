759b0b360b87139e53fb77d4e0e1d819a6b2c7a2
Maksym_Shustyk <shustik2@gmail.com>
2021-11-23 19:05:44 +0200

    Механіка зміни ціни відносно до обраного типу роз.

--- src/components/PizzasBlock/index.jsx
+++ src/components/PizzasBlock/index.jsx
@@ -1,33 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classNames from 'classnames';
 import PropTypes from 'prop-types'
 import Button from '../Button'
 
 
-function PizzasBlock({id, name, imageUrl, sizes, price, types, onClickAddPizza, addedCount}) {
-    const typeNames = ['тонкое','традиционное']
-    const sizeNames = [26,30,40]
+function PizzasBlock({id, name, information, imageUrl, sizes, price, types, onClickAddPizza, addedCount}) {
+    const typeNames = [
+        ['тонкое','традиционное'],
+        [price ,price + 50]
+    ]
+
+    const sizeNames = [
+        [26,30,40],
+        [price ,price + 50, price+100]
+    ]
+
     const [activeType, setActiveType] = React.useState(types[0])
 // Виправити size на index.
     const [activeSize, setActiveSize] = React.useState(sizes[0])
-
-
+    const [activePricePizza, setActivePricePizza] = useState(price)
+    console.log(sizes);
     const onSelectType = (index)=>{
-        setActiveType(index)
+        setActiveType(index);
+        index===0? setActivePricePizza(typeNames[1][index]):
+        setActivePricePizza(typeNames[1][index]);
+
+        if (index===0) {
+            setActiveSize(sizes[0])
+        }else{
+            setActiveSize(sizes[0])
+        }
+
+    }
+    const onSelectSize = (size, index)=>{
+        setActiveSize(size);
+
+        if (typeNames[0][activeType]==='традиционное') {
+
+            size===size?
+                setActivePricePizza(sizeNames[1][index]+50):
+                setActivePricePizza(sizeNames[1][index]+50)
+
+        }else{
+            setActiveSize(size)? setActivePricePizza(sizeNames[1][index]):
+        setActivePricePizza(sizeNames[1][index]);
+        }
     }
 
-    const onSelectSize = (size)=>{
-        setActiveSize(size)
-    }
+
 
     const onAddPizza = ()=>{
         const obj = {
             id,
             name,
             imageUrl,
-            price,
+            price: activePricePizza,
             size: activeSize,
-            type: typeNames[activeType],
+            type: typeNames[0][activeType],
         }
         onClickAddPizza(obj);
     };
@@ -35,10 +64,11 @@ function PizzasBlock({id, name, imageUrl, sizes, price, types, onClickAddPizza, 
         <div className="pizza-block">
                         <img src={imageUrl} alt="Hot pizza img" className="pizza-block_img"/>
                         <h4 className="pizza-block_title">{name}</h4>
+                        <p className="pizza-block_information">{information}</p>
                         <div className="pizza-block_selector">
                             <ul>
                                 {
-                                    typeNames.map((type, index)=>(
+                                    typeNames[0].map((type, index)=>(
                                     <li key={type}
                                     onClick={()=>onSelectType(index)}
                                     className={
@@ -54,10 +84,10 @@ function PizzasBlock({id, name, imageUrl, sizes, price, types, onClickAddPizza, 
                             </ul>
                             <ul>
                                 {
-                                    sizeNames.map((size)=>
+                                    sizeNames[0].map((size, index)=>
 
                                     <li key={size}
-                                    onClick={()=>onSelectSize(size)}
+                                    onClick={()=>onSelectSize(size, index)}
                                     className={
                                         classNames({
                                             'active': activeSize===size,
@@ -71,7 +101,9 @@ function PizzasBlock({id, name, imageUrl, sizes, price, types, onClickAddPizza, 
                         </div>
 
                         <div className="pizza-block_bottom">
-                            <div className="pizza-block_price">{price} ₴</div>
+                            <div className="pizza-block_price">{
+                                activePricePizza
+                                } ₴</div>
                             <Button onClick={onAddPizza}  className="pizza-block_bottom-btn" circle>
                                 <svg width="12" height="12" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"></path>
